612695e97feaaa6af9b17198707c11c72a55245a
LASK-PC\Lau <lask@cs.au.dk>
2018-01-27 23:23:18 +0100

    Working intro with transition to main game loop. Still needs some work.

--- Boundless/Assets/ColliderEventReporter.cs
+++ Boundless/Assets/ColliderEventReporter.cs
@@ -3,9 +3,15 @@
 public class ColliderEventReporter : MonoBehaviour {
 
 	public Signal OnClickedSignal = new Signal();
+	public Signal OnTriggerSignal = new Signal();
 
 	public void OnMouseUpAsButton()
 	{
 		OnClickedSignal.Dispatch();
+	}
+
+	public void OnTriggerEnter2D(Collider2D c)
+	{
+		OnTriggerSignal.Dispatch ();
 	}
 }
