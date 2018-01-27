166b747e1dad07c4ed83efd9103df6531d90f74c
LASK-PC\Lau <lask@cs.au.dk>
2018-01-27 14:44:26 +0100

    Background fix

--- Boundless/Assets/BackgroundBehaviour.cs
+++ Boundless/Assets/BackgroundBehaviour.cs
@@ -12,23 +12,24 @@ public class BackgroundBehaviour : MonoBehaviour {
 	private float height;
 	// Use this for initialization
 	void Start () {
-		background1.transform.position = new Vector2 (0,-7);
+		background1.transform.position = new Vector2 (0,7);
 		Sprite spr1 = (background1.GetComponent<SpriteRenderer> ()).sprite;
 		height = spr1.bounds.max.y;
-		background2.transform.position = new Vector2 (0, height - 7);
+		Debug.Log (height);
+		background2.transform.position = new Vector2 (0, 7 - height);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 trans = new Vector2 (0, -1*speed);
+		Vector2 trans = new Vector2 (0, speed);
 		background1.transform.Translate (trans);
 		background2.transform.Translate (trans);
-		if (background1.transform.position.y < -7.0-height) {
-			background1.transform.position =  new Vector2 (0,background2.transform.position.y + height);
+		if (background1.transform.position.y > 7.0) {
+			background1.transform.position =  new Vector2 (0,background2.transform.position.y - height);
 			Debug.Log (1);
 		}
-		if (background2.transform.position.y < -7.0-height) {
-			background2.transform.position =  new Vector2 (0,background1.transform.position.y + height);
+		if (background2.transform.position.y > 7.0) {
+			background2.transform.position =  new Vector2 (0,background1.transform.position.y - height);
 			Debug.Log (2);
 		}
 	}
