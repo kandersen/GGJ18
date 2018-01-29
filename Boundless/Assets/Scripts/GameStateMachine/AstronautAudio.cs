4e9258d485e456459e7d513ce162a6f5324e2593
LASK-PC\Lau <lask@cs.au.dk>
2018-01-29 12:07:15 +0100

    Added animation to astronaut arms

---
+++ Boundless/Assets/Scripts/GameStateMachine/AstronautAudio.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AstronautAudio : MonoBehaviour
+{
+    public AudioClip PickupBatterySound;
+    public AudioClip FlickSwitchSound;
+    public AudioClip CombineSuccessSound;
+    public AudioClip CombineFailureSound;
+
+    public AudioSource AudioSource;
+    
+    public void PlayPickupBattery()
+    {
+        AudioSource.PlayOneShot(PickupBatterySound);
+    }
+
+    public void PlayFlickSwitch()
+    {
+        AudioSource.PlayOneShot(FlickSwitchSound);
+    }
+
+    public void PlayCombineSuccess()
+    {
+        AudioSource.PlayOneShot(CombineSuccessSound);
+    }
+
+    public void PlayCombineFailure()
+    {
+        AudioSource.PlayOneShot(CombineFailureSound);
+    }
+}
