3a1ca759804f7acfebbb2f16bbd07309580ededc
LASK-PC\Lau <lask@cs.au.dk>
2018-01-29 11:26:39 +0100

    small changes for better feel

---
+++ Boundless/Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkScript : MonoBehaviour
+{
+    public SpriteRenderer Eyes;
+
+    public IEnumerator Start()
+    {
+        var timeToNextBlink = Random.value * 8.0f;
+        while (true)
+        {
+            timeToNextBlink -= Time.deltaTime;
+            if (timeToNextBlink < 0f)
+            {
+                Eyes.enabled = false;
+                yield return new WaitForSeconds(0.1f);
+                Eyes.enabled = true;
+                timeToNextBlink = Random.value * 8.0f;
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Boundless/Assets/Scripts/ItemFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFactory : MonoBehaviour
+{
+    public GameplayController GameplayController;
+    public List<ItemBehaviour> ItemPrefabs =  new List<ItemBehaviour>();
+
+    public ItemBehaviour SpawnItem()
+    {
+        int index = Random.Range(0, ItemPrefabs.Count);
+        var prefab = ItemPrefabs[index];
+        var result = Instantiate(prefab);
+        result.GameplayController = GameplayController;
+        return result;
+    }
+}
