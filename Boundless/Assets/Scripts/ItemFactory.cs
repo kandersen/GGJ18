879acf8b69660c4899e2b540f57988609574fe5e
LASK-PC\Lau <lask@cs.au.dk>
2018-02-01 22:49:11 +0100

    Fixed layer issue and added categories to ItemFactory, so it is easier to make more involved item spaws.

--- Boundless/Assets/Scripts/ItemFactory.cs
+++ Boundless/Assets/Scripts/ItemFactory.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 public class ItemFactory : MonoBehaviour
 {
     public GameplayController GameplayController;
-    public List<ItemBehaviour> ItemPrefabs =  new List<ItemBehaviour>();
+	public List<ItemBehaviour> GarbagePrefabs =  new List<ItemBehaviour>();
+	public List<ItemBehaviour> ButtonPrefabs =  new List<ItemBehaviour>();
+	public List<ItemBehaviour> AntennaPrefabs =  new List<ItemBehaviour>();
+	public List<ItemBehaviour> PowerSourcePrefabs =  new List<ItemBehaviour>();
+
+	private int ItemCount;
+
+	public void Start()
+	{
+		ItemCount = GarbagePrefabs.Count + ButtonPrefabs.Count + AntennaPrefabs.Count + PowerSourcePrefabs.Count;
+	}
 
     public ItemBehaviour SpawnItem()
     {
-        int index = Random.Range(0, ItemPrefabs.Count);
-        var prefab = ItemPrefabs[index];
+        int index = Random.Range(0, ItemCount);
+		var prefab = PickItem(index);
         var result = Instantiate(prefab);
         result.GameplayController = GameplayController;
         return result;
     }
+
+	private ItemBehaviour PickItem(int index)
+	{
+		if (index < GarbagePrefabs.Count) {
+			return GarbagePrefabs [index];
+		} else if (index < GarbagePrefabs.Count + ButtonPrefabs.Count) {
+			return ButtonPrefabs [index - GarbagePrefabs.Count];
+		} else if (index < GarbagePrefabs.Count + ButtonPrefabs.Count + AntennaPrefabs.Count) {
+			return AntennaPrefabs [index - GarbagePrefabs.Count - ButtonPrefabs.Count];
+		} else {
+			return PowerSourcePrefabs [index - GarbagePrefabs.Count - ButtonPrefabs.Count - AntennaPrefabs.Count];
+		}
+	}
 }
