de172c13316cf7cd641335f72bb6bd8ab59db381
Kristoffer Just Andersen <kja@cs.au.dk>
2018-02-04 22:49:51 +0000

    Added the music manager, possibly proper singleton pattern

---
+++ Boundless/Assets/BackgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+	public static BackgroundMusic Instance;
+
+	public AudioSource AudioSource;
+
+	public void Start () {
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	public void Play(AudioClip music)
+	{
+		AudioSource.clip = music;
+		AudioSource.Play();
+	}
+}
