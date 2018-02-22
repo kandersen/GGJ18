77cfb5425727fced930ac56a845e53d76353aaf7
Kristoffer Just Andersen <kja@cs.au.dk>
2018-02-22 23:30:59 +0000

    Clean up

--- Boundless/Assets/Scripts/ManagerLocatorPrefabs.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu]
-public class ManagerLocatorPrefabs : ScriptableObject
-{
-    public SoundManager SoundManager;
-    public PersistentDataManager PersistentDataManager;
-}
